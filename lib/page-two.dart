c87a6d86a9ad4714e624fb776d18397443ef344c
Sana Pathan <sanapathan28@gmail.com>
2024-05-22 11:07:15 -0700

    animation

--- lib/page-two.dart
+++ lib/page-two.dart
@@ -16,12 +16,21 @@ class PageTwo extends StatefulWidget {
   _PageTwoState createState() => _PageTwoState();
 }
 
-class _PageTwoState extends State<PageTwo> {
+class _PageTwoState extends State<PageTwo> with SingleTickerProviderStateMixin {
+
   Timer? _timer;
   int _start = 60;
   int count = 0;
   FlutterTts flutterTts = FlutterTts();
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+  bool _isAnimationVisible = true;
 
+  Future<void> stage_finished() async {
+    speakMessage("You have found all occurrences of number 1");
+    print("AppScore");
+    print(AppScore().currentScore);
+  }
   Future<void> speakMessage(String messageKey) async {
     String languageCode = AppLanguage().currentLanguage;
     String data =
@@ -63,8 +72,10 @@ class _PageTwoState extends State<PageTwo> {
       (Timer timer) {
         if (_start == 0) {
           setState(() {
-            print("Timer Completed"); // Debug when timer reaches 0
+            count = 0; // Reset the counter
+            buttonClicked = {0: false, 1: false, 2: false, 3: false}; // Reset the buttons
             timer.cancel();
+            resetTimer(); // Restart the timer
           });
         } else {
           setState(() {
@@ -78,17 +89,35 @@ class _PageTwoState extends State<PageTwo> {
   @override
   void initState() {
     super.initState();
-    startTimer();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
-        AppScore().setStageScore(2, 0);
+        AppScore().setStageScore(1, 0);
       });
+    });
+
+    // Initialize the animation controller and animation
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0.0, -1.0),
+      end: Offset(0.0, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    // Start the animation and play the sound
+    _controller.forward().then((_) {
+      speakMessage("page_two_message");
     });
   }
 
   @override
   void dispose() {
     _timer?.cancel();
+    _controller.dispose();
     super.dispose();
   }
 
@@ -122,13 +151,16 @@ class _PageTwoState extends State<PageTwo> {
               Positioned(
                 bottom: MediaQuery.of(context).size.height * 0.02,
                 left: MediaQuery.of(context).size.width * 0.25,
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.refresh,
-                    size: 60,
+                child: Tooltip(
+                  message: 'Reset', // Tooltip message
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.refresh,
+                      size: 60,
+                    ),
+                    color: Colors.yellow,
+                    onPressed: resetCountAndButtons,
                   ),
-                  color: Colors.yellow,
-                  onPressed: resetCountAndButtons,
                 ),
               ),
               Positioned(
@@ -177,10 +209,7 @@ class _PageTwoState extends State<PageTwo> {
                   color: Colors.yellow,
                   onPressed: () async {
                     if (allButtonsClicked()) {
-                      speakMessage(
-                          "You have found all occurrences of number 4");
-                      print("AppScore");
-                      print(AppScore().currentScore);
+                      stage_finished();
                     }
                     // Speak the hint if the button hasn't been clicked
                     for (int i = 0; i < buttonToHint.length; i++) {
@@ -255,7 +284,41 @@ class _PageTwoState extends State<PageTwo> {
                 },
               ),
               ScoreWidget(),
-              LanguageWidget()
+              LanguageWidget(),
+              // Animation Overlay
+              if (_isAnimationVisible)
+                Positioned.fill(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ClipOval(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            height: MediaQuery.of(context).size.width * 0.5,
+                            child: SlideTransition(
+                              position: _offsetAnimation,
+                              child: Image.asset(
+                                'assets/two.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _isAnimationVisible = false;
+                            });
+                            startTimer(); // Start the timer when OK is pressed
+                          },
+                          child: Text("OK"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
             ],
           );
         },
@@ -288,11 +351,13 @@ class _PageTwoState extends State<PageTwo> {
               setState(() {
                 count++;
                 buttonClicked[index] = true;
-                AppScore().setStageScore(2, AppScore().getStageScore(2)! + 100);
+                AppScore().setStageScore(1, AppScore().getStageScore(1)! + 100);
                 if (count == 4) {
                   _showStarsDialog();
-                  flutterTts
-                      .speak("You have found all occurrences of number 2");
+                  flutterTts.speak(
+                      "Congratulations!! You have found all occurrences of number 1");
+                  print("AppScore");
+                  print(AppScore().currentScore);
                 }
               });
             }
@@ -308,11 +373,23 @@ class _PageTwoState extends State<PageTwo> {
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: Colors.blueGrey, // Change background color
-          title: const Text(
-            'Congratulations!',
-            style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 30), // Change text color and size
+          title: Column(
+            children: [
+              const Text(
+                'Congratulations!',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'Your Score: ${AppScore().currentScore}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
           content: const Row(
             mainAxisSize: MainAxisSize.min,
@@ -337,8 +414,9 @@ class _PageTwoState extends State<PageTwo> {
               child: const Text(
                 'Next Level',
                 style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18), // Change button text color and size
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
               ),
             ),
           ],
